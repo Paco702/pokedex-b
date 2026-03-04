a4963b362b6856e187e946b5ac513fb8f4f730a8
Paco702 <up240442@alumnos.upa.edu.mx>
2026-03-04 17:36:12 -0600

    show pokemon on screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+
+  const [results, setResults] = useState<any[]>([]);
 
   useEffect(() => {
     console.log("entré en pantalla");
@@ -9,15 +11,21 @@ export default function Index() {
   }, []);
 
   const getPokemons = async () => {
-    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
     const response = await fetch(URL, {
       method: "GET",
     });
-    console.log(response)
+    console.log(response);
+    const data = await response.json();
+    setResults(data.results);
+    console.log(data);
   };
   return (
     <View>
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      {results.map((pokemon) => (
+        <Text key={pokemon.name}>{pokemon.name} </Text>
+      ))}
     </View>
   );
 }
